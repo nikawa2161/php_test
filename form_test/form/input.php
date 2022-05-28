886ce025da5919588719055e6aab008a8a27cf4f
nikawadori <nikawa.2161.t@gmail.com>
2022-05-28 15:25:16 +0900

    validator1

--- form_test/form/input.php
+++ form_test/form/input.php
@@ -7,9 +7,9 @@ header("X-FRAME-OPTIONS: DENY");
 
 // スーパーグローバル変数 php9種類あり
 // 連想配列
-if(!empty($_SESSION)) {
+if(!empty($_POST)) {
   echo "<pre>";
-  var_dump($_SESSION);
+  var_dump($_POST);
   echo "</pre>";
 }
 
@@ -64,9 +64,40 @@ if(!empty($_POST['btn_submit'])) {
       
       メールアドレス
       <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);} ?>" >
-      
       <br>
       
+      ホームページ
+      <input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']);} ?>" >
+      <br>
+      性別
+      <input type="radio" name="gender" value="0" 
+      <?php if(isset($_POST['gender']) && $_POST['gender'] === '0' ){
+        echo 'checked';
+        } ?>>男性
+      <input type="radio" name="gender" value="1" 
+      <?php if(isset($_POST['gender']) && $_POST['gender'] === '1' ){
+        echo 'checked';
+        } ?>>女性
+      <br>
+      年齢
+      <select name="age" id="">
+        <option value="">選択してください</option>
+        <option value="1" selected>〜19歳</option>
+        <option value="2">20歳〜29歳</option>
+        <option value="3">30歳〜39歳</option>
+        <option value="4">40歳〜49歳</option>
+        <option value="5">50歳〜59歳</option>
+        <option value="6">60歳〜</option>
+      </select>
+      <br>
+      お問合せ内容
+      <textarea name="contact">
+        <?php if(!empty($_POST['contact'])){echo h($_POST['contact']);} ?>
+      </textarea>
+      <br>
+      <input type="checkbox" name="caution" value="1">注意事項にチェックする
+      <br>
+
       <input type="submit" name="btn_confirm" value="確認する">
       <input type="hidden" name="csrf" value="<?php echo $token; ?>">
       
@@ -84,7 +115,27 @@ if(!empty($_POST['btn_submit'])) {
           
           メールアドレス
           <?php echo h($_POST["email"]);?>
-          
+          <br>
+          ホームページ
+          <?php echo h($_POST["url"]);?>
+          <br>
+          性別
+          <?php
+            if($_POST['gender'] === '0') {echo '男性';}
+            if($_POST['gender'] === '1') {echo '女性';}
+            ?>
+          <br>
+          年齢
+          <?php
+            if($_POST['age'] === '1') {echo '〜19歳';}
+            if($_POST['age'] === '2') {echo '20歳〜29歳';}
+            if($_POST['age'] === '3') {echo '30歳〜39歳';}
+            if($_POST['age'] === '4') {echo '40歳〜49歳';}
+            if($_POST['age'] === '5') {echo '50歳〜59歳';}
+            if($_POST['age'] === '6') {echo '60歳';}
+          ?>
+          お問合せ内容
+          <?php echo h($_POST["contact"]);?>
           <br>
           
           <input type="submit" name="back" value="戻る">
@@ -92,6 +143,11 @@ if(!empty($_POST['btn_submit'])) {
           <!-- GET,POSTなどの通信を行うとvalue内容が消える -->
           <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
           <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+          <input type="hidden" name="url" value="<?php echo h($_POST['url']); ?>">
+          <input type="hidden" name="gender" value="<?php echo h($_POST['gender']); ?>">
+          <input type="hidden" name="age" value="<?php echo h($_POST['age']); ?>">
+          <input type="hidden" name="contact" value="<?php echo h($_POST['contact']); ?>">
+          
           <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
         </form>
       <?php endif; ?>
