4e6caf1d1fb587fcbd58c84b55d434386a0f825e
nikawadori <nikawa.2161.t@gmail.com>
2022-05-28 13:16:54 +0900

    get.post_test

--- form_test/form/input.php
+++ form_test/form/input.php
@@ -1,3 +1,15 @@
+<?php
+
+// スーパーグローバル変数 php9種類あり
+// 連想配列
+if(!empty($_GET)) {
+  echo "<pre>";
+  var_dump($_GET);
+  echo "</pre>";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,6 +19,21 @@
   <title>フォーム作成</title>
 </head>
 <body>
-  
+  <!-- methodはGETかPOST actionは処理するファイル名 -->
+  <!-- 送信するとURLの後に?~~~になる。query(クエリ) -->
+  <form method="" action="input.php">
+    氏名
+    <input type="text" name="your_name">
+    <br>
+
+    <input type="checkbox" name="sports[]" value="野球">野球
+    <input type="checkbox" name="sports[]" value="サッカー">サッカー
+    <input type="checkbox" name="sports[]" value="バスケ">バスケ
+
+    <br>
+
+    <input type="submit" value="送信">
+
+  </form>
 </body>
 </html>
