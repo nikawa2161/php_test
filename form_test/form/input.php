59f6f69b86db469dbd337de2e929067259388652
nikawadori <nikawa.2161.t@gmail.com>
2022-05-28 14:12:38 +0900

    feat: create backFunction

--- form_test/form/input.php
+++ form_test/form/input.php
@@ -42,11 +42,11 @@ if(!empty($_POST['btn_submit'])) {
     <!-- 送信するとURLの後に?~~~になる。query(クエリ) -->
     <form method="POST" action="input.php">
       氏名
-      <input type="text" name="your_name">
+      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];} ?>">
       <br>
       
       メールアドレス
-      <input type="email" name="email">
+      <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'];} ?>" >
       
       <br>
       
@@ -59,19 +59,20 @@ if(!empty($_POST['btn_submit'])) {
     <!-- 確認画面--------------------------- -->
     <?php if($pageFlag === 1) : ?>
       <form method="POST" action="input.php">
-      氏名
-      <?php echo $_POST["your_name"];?>
-      <br>
-      
-      メールアドレス
-      <?php echo $_POST["email"];?>
-
-      <br>
-
-      <input type="submit" name="btn_submit" value="送信する">
-      <!-- GET,POSTなどの通信を行うとvalue内容が消える -->
-      <input type="hidden" name="your_name" value="$_POST['your_name']">
-      <input type="hidden" name="email" value="$_POST['email']">
+        氏名
+        <?php echo $_POST["your_name"];?>
+        <br>
+        
+        メールアドレス
+        <?php echo $_POST["email"];?>
+        
+        <br>
+        
+        <input type="submit" name="back" value="戻る">
+        <input type="submit" name="btn_submit" value="送信する">
+        <!-- GET,POSTなどの通信を行うとvalue内容が消える -->
+        <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
       </form>
       <?php endif; ?>
 
